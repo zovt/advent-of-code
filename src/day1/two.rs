cd98635cca8af62cfd27dd799011d5eaeeec1687
zovt <zovt@posteo.de>
2017-12-20 14:23:34 -0500

    Day 1

---
+++ src/day1/two.rs
@@ -0,0 +1,13 @@
+pub fn main() {
+	let num_string = std::env::args().nth(1).unwrap();
+	let num_bytes = num_string.as_str().as_bytes();
+	let len = num_bytes.len();
+	let mut sum: u64 = 0;
+	for i in 0..len {
+		let (fst, snd) = (num_bytes[i] as u8, num_bytes[(i + (len / 2)) % len] as u8);
+		if fst == snd {
+			sum = sum + (fst - ('0' as u8)) as u64;
+		}
+	}
+	println!("{}", sum);
+}
